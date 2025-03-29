70ddf549c679a13d7d40f8a1b122ce6ad116909c
WeaverOfInfinity <nukukwami@outlook.com>
2025-03-29 06:40:16 +0000

    Added a test to get all products

---
+++ backend/src/test/product.test.js
@@ -0,0 +1,33 @@
+const { connectDB, closeDB, clearDB } = require('../utils/testHandler');
+const request = require('supertest');
+const app = require('../../server');
+const Product = require('../models/Product');
+
+beforeAll(async () => {
+    await connectDB();
+});
+
+afterEach(async () => {
+    await clearDB();
+});
+
+afterAll(async () => {
+    await closeDB();
+});
+
+describe('Product API', () => {
+
+    test('GET /products - should return all products', async () => {
+        const product1 = new Product({ name: 'Product 1', description: 'Description 1', price: 100, category: 'Category 1' });
+        const product2 = new Product({ name: 'Product 2', description: 'Description 2', price: 200, category: 'Category 2' });
+        await product1.save();
+        await product2.save();
+
+        const response = await request(app).get('/products');
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.length).toBe(2);
+        expect(response.body[0].name).toBe('Product 1');
+        expect(response.body[1].name).toBe('Product 2');
+    });
+});
