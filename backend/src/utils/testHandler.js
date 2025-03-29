bad62498b8d31ec487d67026db2055cbbe2cf669
WeaverOfInfinity <nukukwami@outlook.com>
2025-03-29 14:16:01 +0000

    Removed console logging from test handler

--- backend/src/utils/testHandler.js
+++ backend/src/utils/testHandler.js
@@ -10,15 +10,13 @@ const connectDB = async () => {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
-    console.log('MongoDB connected to memory server');
 }
 
 const closeDB = async () => {
     await mongoose.connection.dropDatabase();
     await mongoose.connection.close();
-    console.log('MongoDB connection closed');
+
     await mongoServer.stop() // stop the memory server to allow Jest to exit
-    console.log('MongoDB memory server stopped');
 }
 
 const clearDB = async () => {
@@ -26,7 +24,6 @@ const clearDB = async () => {
     for (const key in collections) {
         await collections[key].deleteMany({});
     }
-    console.log('MongoDB database cleared');
 }
 
 module.exports = { connectDB, closeDB, clearDB };
