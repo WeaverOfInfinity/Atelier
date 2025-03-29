b73b290e0d26595a2c1c9e15562d41b1e01aa51a
WeaverOfInfinity <nukukwami@outlook.com>
2025-03-29 06:49:47 +0000

    Added an endpoint to get products by id

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -9,5 +9,21 @@ module.exports = {
         } catch (error) {
             res.status(500).json({ message: 'Error fetching products', error });
         }
+    },
+
+    getProductById: async (req, res) => {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'Invalid product ID'});
+        } 
+        try {
+            const product = await Product.findById(id);
+            if (!product) {
+                return res.status(404).json({ message: 'Product not found' });
+            }
+            res.status(200).json(product);
+        } catch (error) {
+            res.status(500).json({ message: 'Error fetching product', error });
+        }
     }
 }
