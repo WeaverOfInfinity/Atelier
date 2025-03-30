17b02ef0bfe750d5a0da99af5028b42ad1833184
WeaverOfInfinity <nukukwami@outlook.com>
2025-03-30 18:12:20 +0000

    Added an endpoint for deleting products

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -162,4 +162,24 @@ module.exports = {
             });
         }
     },
+
+    deleteProduct: async (req, res) => {
+        const { id } = req.params;
+            
+        // Validate ID format
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'Invalid product ID' });
+        }
+        
+        try {
+            const deletedProduct = await Product.findByIdAndDelete(id);
+            if (!deletedProduct) {
+                return res.status(404).json({ message: 'Product not found' });
+            }
+            
+            return res.status(200).json({ message: 'Product deleted successfully' });
+        } catch (error) {
+            return res.status(500).json({ message: 'Error deleting product', error });
+        }
+    }
 }
--- backend/src/routes/productRouter.js
+++ backend/src/routes/productRouter.js
@@ -22,4 +22,6 @@ router.post('/', productController.createProduct);
 
 router.put('/:id', productController.updateProduct);
 
+router.delete('/:id', productController.deleteProduct);
+
 module.exports = router;
