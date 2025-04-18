9d5320b0229163c097e3535762e68629c6ecc9a0
WeaverOfInfinity <nukukwami@outlook.com>
2025-04-18 08:14:20 +0000

    Added a security group for Frontend ALB

--- infra/lib/network-stack.js
+++ infra/lib/network-stack.js
@@ -18,10 +18,31 @@ export class NetworkStack extends cdk.Stack {
       ],
       natGateways: 0, // No private subnets or NAT gateways
     });
+
+    // === FRONTEND ALB SECURITY GROUP ===
+    this.frontendAlbSG = new ec2.SecurityGroup(this, 'FrontendALBSG', {
+      vpc,
+      description: 'Allows public traffic on HTTP and HTTPS for frontend ALB',
+      allowAllOutbound: true, // Allow all outbound traffic (default is true)
+    });
+
+    this.frontendAlbSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'Allow HTTP traffic from anywhere'
+    );
+
+    this.frontendAlbSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'Allow HTTPS traffic from anywhere'
+    );
     
     // Tag the VPC
     cdk.Tags.of(this.vpc).add('Name', 'AtelierVPC');
     cdk.Tags.of(this.vpc).add('Realm', 'Networking');
+    cdk.Tags.of(this.frontendAlbSG).add('Name', 'AtelierFrontendALBSG');
+    cdk.Tags.of(this.frontendAlbSG).add('Realm', 'Networking');
     
     // Export VPC and subnet IDs needed by other stacks
     new cdk.CfnOutput(this, 'VPCId', {
@@ -43,6 +64,12 @@ export class NetworkStack extends cdk.Stack {
     new cdk.CfnOutput(this, 'PublicSubnetCId', {
       value: publicSubnets[2].subnetId,
       exportName: 'AtelierPublicSubnetCId',
+    });
+
+    // Export SG IDs for other stacks
+    new cdk.CfnOutput(this, 'FrontendALBSGId', {
+      value: this.frontendAlbSG.securityGroupId,
+      exportName: 'FrontendALBSGId',
     });
   }
 }
