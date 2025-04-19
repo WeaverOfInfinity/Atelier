3562442078b65ce6c4265709a795d7a0a059d8b5
WeaverOfInfinity <nukukwami@outlook.com>
2025-04-19 13:14:12 +0000

    Added file name changes

--- infra/bin/atelier.js
+++ infra/bin/atelier.js
@@ -1,19 +1,18 @@
+// bin/atelier.js
 import { App } from 'aws-cdk-lib';
 import { NetworkStack } from '../lib/network-stack.js';
 import { DatabaseStack } from '../lib/database-stack.js';
 import { BackendStack } from '../lib/backend-stack.js';
 
 const app = new App();
+const env = { 
+  account: process.env.CDK_DEFAULT_ACCOUNT, 
+  region: process.env.CDK_DEFAULT_REGION 
+};
 
-const networkStack = new NetworkStack(app, 'NetworkStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-});
+const networkStack = new NetworkStack(app, 'NetworkStack', { env });
+new DatabaseStack(app, 'DatabaseStack', { env });
 
-new DatabaseStack(app, 'DatabaseStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-});
-
-const backendStack = new BackendStack(app, 'BackendStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  networkStack: networkStack,
-});
+// Create a single backend stack that contains both ALB and ASG
+const backendStack = new BackendStack(app, 'BackendStack', { env });
+backendStack.addDependency(networkStack);
