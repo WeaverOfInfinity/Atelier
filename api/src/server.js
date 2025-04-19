1f265f648430b1fe6be500f674f1e17c3a08dded
WeaverOfInfinity <nukukwami@outlook.com>
2025-04-19 13:13:06 +0000

    Exposed api to internet

--- api/src/server.js
+++ api/src/server.js
@@ -6,6 +6,6 @@ dotenv.config(); // Load environment variables
 const PORT = process.env.PORT || 5000; // Default to port 3000 if not specified in .env
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
